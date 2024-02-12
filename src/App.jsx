500458943bc7d904591d56c44dc8c872eb479378
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-02-12 18:54:26 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home'
 // import Navbar from './Components/Navbar'
-// import Stories from './Components/Stories'
-// import Galler from './Components/Galler'
+import Stories from './Components/Stories'
+import Galler from './Components/Galler'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
@@ -23,7 +23,7 @@ function App() {
           <Route>
             <Route path='/' element={<Home />} />
             <Route path='/stories' element={<Stories />} />
-            {/* <Route path='/galler' element={<Galler />} /> */}
+            <Route path='/galler' element={<Galler />} />
            
             
            
